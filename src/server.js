3898128e679b8a70be9ff0a54e17a01805b54b87
Amisha <bernadethguzom37@gmail.com>
2026-04-27 22:09:30 +0800

    initial commit

---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(rateLimiter);
+app.use(express.json());
+/*app.use((req,res,next) => {
+    console.log("Hey we hit a req, the method is",req.method);
+    next();
+});*/
+
+const PORT = process.env.PORT || 8080;
+
+//connectDB(process.env.DATABASE_URL);
+
+/*app.get("/health",(req,res) => {
+    res.send("It's working");
+});*/
+
+app.use("/api/transactions", transactionsRoute);
+
+
+
+/*app.get("/",(req, res) =>{
+    res.send("It's Working");
+});*/
+
+//console.log("my port:", process.env.PORT);
+
+initDB().then(() => {
+    app.listen(PORT,() => {
+    console.log("Server is up and running on PORT:", PORT);
+});
+});
