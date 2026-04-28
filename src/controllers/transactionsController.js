8dabbe1f1a378500907405feb578d98cb710cf8c
bernadethguzom37-lgtm <bernadethguzom37@gmail.com>
2026-04-28 12:00:09 +0800

    Update transactionsController.js

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -2,7 +2,7 @@ import { sql } from "../config/db.js";
 
 export async function getTransactionsByUserId(req, res) {
         try {
-            const { userId } = req.params;
+            const { user_id } = req.params;
     
             const transactions= await sql`
             SELECT * FROM transactions WHERE user_id = ${user_id} ORDER BY created_at DESC
